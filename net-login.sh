adbb6c74008ecdea06ebe8955689df524e5daafe
Sandro Pischinger <mail@sandropischinger.de>
2025-05-31 13:34:47 +0200

    add dns resolve

--- net-login.sh
+++ net-login.sh
@@ -10,31 +10,41 @@ set -euo pipefail
 
 trm_useragent="Mozilla/5.0 (Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0"
 trm_maxwait="5000"
+trm_neededprofile="6"
+trm_vodafonedns="10.175.177.132"
+trm_resolve="hotspot.vodafone.de:443:10.175.177.131"
+SESSIONFILE="/tmp/bayernhotspot.session"
 
-# Pings quadnine dns server (privacy respecting). Returns "online" if connected to network and offline otherwise
-if ping -q -c1 9.9.9.9 &>/dev/null; then
+if ping -q -c1 "${trm_vodafonedns}" &>/dev/null; then
     network_status=online
 else
    network_status=offline
 fi
 
-# Reconnect if offline
 if [ "$network_status" = "offline" ]; then
+  curl -v --resolve "$trm_resolve" https://hotspot.vodafone.de/api/v4/session \
+    --user-agent "${trm_useragent}" --silent \
+    --connect-timeout "${trm_maxwait}" >$SESSIONFILE
 
-  # get session id
-  SESSION_ID=$(curl https://hotspot.vodafone.de/api/v4/session \
-  --user-agent "${trm_useragent}" --silent \
-  --connect-timeout "${trm_maxwait}" | jq -r '.session')
+  SESSION_ID=$(jq -r '.session' $SESSIONFILE)
+  SESSION_PROFILE=$(jq -r '.currentLoginProfile' $SESSIONFILE)
 
-  # login to business_premium profile
-  RESULT=$(curl -H 'Content-Type: application/json' \
+  # fake some dns activity
+  dig "@$trm_vodafonedns" google.com
+  echo "Session-ID: $SESSION_ID"
+
+  if [ "$SESSION_PROFILE" = "$trm_neededprofile" ]; then
+    exit 0
+  fi
+
+  RESULT=$(curl --resolve "$trm_resolve" -H 'Content-Type: application/json' \
   --user-agent "${trm_useragent}" --connect-timeout "${trm_maxwait}" \
-  --request POST --data "{\"loginProfile\": 6, \"accessType\": \"termsOnly\", \"session\": \"${SESSION_ID}\"}"\
+  --request POST --data "{\"loginProfile\": $trm_neededprofile, \"accessType\": \"termsOnly\", \"session\": \"${SESSION_ID}\"}"\
   https://hotspot.vodafone.de/api/v4/login 2>/dev/null | jq -r '.message')
 
   if [ "$RESULT" = "OK" ]; then
-    exit 0
     echo success!
+    exit 0
   else
     echo "$RESULT"
     exit 1
